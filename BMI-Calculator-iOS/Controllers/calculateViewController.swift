99016136e34b685b197deebfb61ad081e80cd2c8
Abhang Mane <maneabhang1@gmail.com>
2024-02-05 15:02:33 +0530

    Initial Commit

---
+++ BMI-Calculator-iOS/Controllers/calculateViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class calculateViewController: UIViewController {
+
+    //setting initial value
+    var bmiValue = "0.0"
+    var calculatorBrain = CalculatorBrain()
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f",sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f",sender.value)
+        weightLabel.text = "\(weight)kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let heightValue = heightSlider.value
+        let weightValue = weightSlider.value
+        calculatorBrain.calculateBMI(height:heightValue ,weight:weightValue)
+        
+        
+        //segue with destination identifier name
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+        
+    }
+    
+    //segue object containing information about the view controllers involved in the segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+        }
+    }
+    
+}
+
